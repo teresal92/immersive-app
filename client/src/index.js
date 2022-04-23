493d68278feeb14d77e72019d357ed6fdf4b0904
Teresa Lew <teresalew92@gmail.com>
2022-04-22 18:01:58 -0700

    installed estlint airbnb style guide

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,4 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './components/App.jsx';
 
 ReactDOM.render(<App /> , document.getElementById('app'));
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<App />} />
-//       <Route path="dashboard" element={<Dashboard />} />
-//     </Routes>
-//   </BrowserRouter>
-//   , document.getElementById('app'));
+
